606bbb50917c6c302e775f75b46ac2d46bf38103
Kunhee Kim <kunheekim@korea.ac.kr>
2023-05-19 01:14:51 +0900

    feat: Embed Message for better readability

--- src/commands/ranking.ts
+++ src/commands/ranking.ts
@@ -1,10 +1,12 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction, codeBlock } from 'discord.js';
+import { CommandInteraction, EmbedBuilder, EmbedField } from 'discord.js';
 import axios from 'axios';
 import { load } from 'cheerio';
 
-async function fetchTeamRankTable(): Promise<string> {
+async function fetchTeamRankTable(): Promise<EmbedBuilder> {
   try {
+
+    // 크롤링
     const response = await axios.get(
       'https://www.koreabaseball.com/TeamRank/TeamRank.aspx'
     );
@@ -20,9 +22,48 @@ async function fetchTeamRankTable(): Promise<string> {
       arr.push(wanted);
     });
 
-    const formattedString = arr.map((row) => row.join('\t')).join('\n');
-    return formattedString;
-    // todo: formatt the string!
+
+    // Embed 메시지로 반환해 가독성 좋게 순위표 출력 
+    const values: string[] = [];
+
+    let index = '';
+    for (let i = 0; i < arr[0].length; i++) {
+      if (arr[0][i].length == 1) index += arr[0][i] + '      ';
+      if (arr[0][i].length == 2) index += arr[0][i] + '     ';
+      else if (arr[0][i].length == 3) index += arr[0][i] + '    ';
+    }
+    values.push('`' + index + '`');
+
+    for (let i = 1; i < arr.length; i++) {
+      let value = '';
+      for (let j = 0; j < arr[i].length; j++) {
+        if (j == 1) {
+          if ( // 한글은 글자 너비가 달라서 줄 간격 맞추려 보정
+            arr[i][j] == '롯데' ||
+            arr[i][j] == '두산' ||
+            arr[i][j] == '키움' ||
+            arr[i][j] == '삼성' ||
+            arr[i][j] == '한화'
+          ) {
+            value += arr[i][j] + ' '.repeat(5);
+          } else {
+            value += arr[i][j] + ' '.repeat(8 - arr[i][j].length);
+          }
+        } else {
+          value += arr[i][j] + ' '.repeat(8 - arr[i][j].length);
+        }
+      }
+      value += '';
+      values.push('`' + value + '`');
+    }
+
+    const field: EmbedField = {
+      name: "KBO 프로야구 순위표",
+      value: values.join('\n'),
+      inline: false,
+    };
+    const embed = new EmbedBuilder().setFields(field);
+    return embed;
   } catch (error) {
     console.error('Error occurred while fetching team rank table:', error);
     throw error;
@@ -36,7 +77,10 @@ module.exports = {
   async execute(interaction: CommandInteraction) {
     fetchTeamRankTable()
       .then(async (result) => {
-        await interaction.reply(codeBlock(result));
+        console.log(result.data.fields);
+        await interaction.reply({
+          embeds: [result],
+        });
       })
       .catch((error) => {
         console.error('Error occurred while fetching team rank table:', error);
