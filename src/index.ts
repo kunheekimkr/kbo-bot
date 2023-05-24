59713bfe5d53942e91c42dbee1957442b57b9efd
Kunhee Kim <kunheekim@korea.ac.kr>
2023-05-24 12:16:16 +0900

    refactor: seperate event files

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import fs = require('fs');
 import path = require('path');
 import dotenv = require('dotenv');
 
-import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
 
 import { Command } from './interfaces/command';
 dotenv.config();
@@ -16,6 +16,13 @@ declare module 'discord.js' {
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith('.ts'));
 
 async function importCommandModules() {
   for (const file of commandFiles) {
@@ -40,47 +47,25 @@ async function importCommandModules() {
   }
 }
 
-client.commands = new Collection();
-
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs
-  .readdirSync(commandsPath)
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs
+  .readdirSync(eventsPath)
   .filter((file) => file.endsWith('.ts'));
 
+async function importEventModules() {
+  for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = await import(filePath);
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args));
+    }
+  }
+}
+
 importCommandModules().then(() => {
-  client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-      console.error(
-        `No command matching ${interaction.commandName} was found.`
-      );
-      return;
-    }
-
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      if (interaction.replied || interaction.deferred) {
-        await interaction.followUp({
-          content: 'There was an error while executing this command!',
-          ephemeral: true,
-        });
-      } else {
-        await interaction.reply({
-          content: 'There was an error while executing this command!',
-          ephemeral: true,
-        });
-      }
-    }
+  importEventModules().then(() => {
+    client.login(process.env.DISCORD_TOKEN);
   });
-
-  client.once(Events.ClientReady, (c: { user: { tag: string } }) => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-  });
-
-  client.login(process.env.DISCORD_TOKEN);
 });
